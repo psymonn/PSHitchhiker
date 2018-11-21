431506c94c69495591204139d088bbdb3d25e00f
psymonn <40018954+psymonn@users.noreply.github.com>
2018-11-21 23:08:27 +1100

    udpate

--- tests/private/Get-RomanNumeral.tests.ps1
+++ tests/private/Get-RomanNumeral.tests.ps1
@@ -11,7 +11,7 @@ Write-Host "file: $sut"
 . "$here\$sut"
 write-host "dot-source: $here\$sut"
 
-Describe "STRY-0001234_Get-RomanNumeral" {
+Describe "Get-RomanNumeral" {
   Context "STRY-0001235_Simple Calculations" {
       It "Calculates the Roman Numeral for 1" {
           Get-RomanNumeral -Number 1 | Should be "I"
