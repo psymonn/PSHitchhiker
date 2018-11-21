31c305dda093f51de3be82fb789f8622352dc1b9
psymonn <40018954+psymonn@users.noreply.github.com>
2018-11-21 23:26:38 +1100

    update

--- tests/private/Get-RomanNumeral.tests.ps1
+++ tests/private/Get-RomanNumeral.tests.ps1
@@ -11,8 +11,8 @@ Write-Host "file: $sut"
 . "$here\$sut"
 write-host "dot-source: $here\$sut"
 
-Describe "STRY-0001234_Get-RomanNumeral" {
-  Context "STRY-0001235_Simple Calculations" {
+Describe "STRY-1234_Get-RomanNumeral" {
+  Context "STRY-1235_Simple Calculations" {
       It "Calculates the Roman Numeral for 1" {
           Get-RomanNumeral -Number 1 | Should be "I"
       }
@@ -53,7 +53,7 @@ Describe "STRY-0001234_Get-RomanNumeral" {
           Get-RomanNumeral -Number 1000 | Should be "M"
       }
     }
-    Context "STRY-0001236_Complex Calculations" {
+    Context "STRY-1236_Complex Calculations" {
         It "Calculates the Roman Numeral for 1999" {
             Get-RomanNumeral -Number 1999 | Should be "MCMXCIX"
         }
